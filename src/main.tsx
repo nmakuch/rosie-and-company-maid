ea939984c46100e334fe365fee8c97305425b1cc
Nick Makuch <makuch.nick@gmail.com>
2026-08-20 19:56:37 -0400

    fade in

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,49 @@
-import './index.css'
-import './Global.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import "./index.css";
+import "./Global.css";
 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App";
+
+const rootElement =
+  document.getElementById("root")!;
+
+createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
+
+const fontLoadingTimeout = new Promise<void>((resolve) => {
+  window.setTimeout(resolve, 1500);
+});
+
+const requiredFonts = document.fonts
+  ? Promise.all([
+    document.fonts.load(
+      '400 1rem "Google Sans"'
+    ),
+    document.fonts.load(
+      '600 1rem "Google Sans"'
+    ),
+  ]).catch(() => undefined)
+  : Promise.resolve();
+
+void Promise.race([
+  requiredFonts,
+  fontLoadingTimeout,
+]).then(() => {
+  window.requestAnimationFrame(() => {
+    document.documentElement.classList.remove(
+      "fonts-loading"
+    );
+
+    document.documentElement.classList.add(
+      "fonts-ready"
+    );
+  });
+});
