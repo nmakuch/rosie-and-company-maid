2f10ef9124a886e05b721326190574a0a16dc346
Nick Makuch <makuch.nick@gmail.com>
2026-08-31 18:03:57 -0400

    fixed fouc

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-const rootElement =
-  document.getElementById("root")!;
+const rootElement = document.getElementById("root")!;
 
 createRoot(rootElement).render(
   <StrictMode>
@@ -17,42 +16,3 @@ createRoot(rootElement).render(
     </BrowserRouter>
   </StrictMode>,
 );
-
-const fontLoadingTimeout =
-  new Promise<void>((resolve) => {
-    window.setTimeout(resolve, 1500);
-  });
-
-const requiredFonts = document.fonts
-  ? Promise.all([
-    document.fonts.load(
-      '400 1rem "Google Sans"'
-    ),
-    document.fonts.load(
-      '600 1rem "Google Sans"'
-    ),
-  ])
-    .then(() => undefined)
-    .catch(() => undefined)
-  : Promise.resolve();
-
-function revealApp() {
-  document.documentElement.classList.remove(
-    "fonts-loading"
-  );
-
-  document.documentElement.classList.add(
-    "fonts-ready"
-  );
-}
-
-void Promise.race([
-  requiredFonts,
-  fontLoadingTimeout,
-]).then(() => {
-  window.requestAnimationFrame(() => {
-    window.requestAnimationFrame(
-      revealApp
-    );
-  });
-});
